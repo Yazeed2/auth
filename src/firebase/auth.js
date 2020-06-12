fd66b03a7ba1a476245ca2bbb72be6ecd79eba11
Yazeed aljuhani <yazeedmalmarwani@gmail.com>
2020-06-12 16:46:37 +0300

    cleaned some code

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,10 +1,10 @@
 
 import {auth, firestore} from './firebase';
 import {alertError, alertSuccess} from '../components/alerts/alerts'; 
-import {registerCompleted} from '../components/alerts/Messages/success'
-// import {networkError} from '../components/alerts/Messages/error'
+import {registerCompleted} from '../components/alerts/Messages/success';
+
 export const register = async (userInfo) => {
-    const {email, password} = userInfo
+    const {email, password} = userInfo;
     auth.createUserWithEmailAndPassword(email, password)
     .then(userSnap => {
         delete userInfo.password; 
@@ -12,30 +12,23 @@ export const register = async (userInfo) => {
         const uid = userSnap.user.uid;
         firestore.collection('users').doc(uid).set({...userInfo})
         .then(done =>{
-            alertSuccess(registerCompleted)
-            Promise.resolve(true)
+            alertSuccess(registerCompleted);
+            Promise.resolve(true);
         })
         .catch(err=> {
             console.log('%c '+ err.message, 'background: #000; color: red');
-            alertError(err.message)
-            Promise.reject(new Error(err.message))
+            alertError(err.message);
+            Promise.reject(new Error(err.message));
 
         })
-        
-
-        
-        
+ 
     }) 
     .catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
         console.log(errorMessage);
-        
-        alertError(errorMessage); 
-    
+        alertError(errorMessage);
         // ...
       });
-
-
 }
